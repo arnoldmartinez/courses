b4f30e05e40427cf0808c98082b16bae32ff114e
Arnold Martinez <arnoldmartinez@criptext.com>
2023-06-14 23:15:40 -0600

    TextFields

--- the_complete_android_oreo_developer_course_build_23_apps/InteractivityDemo/app/src/main/java/com/example/interactivitydemo/MainActivity.java
+++ the_complete_android_oreo_developer_course_build_23_apps/InteractivityDemo/app/src/main/java/com/example/interactivitydemo/MainActivity.java
@@ -3,13 +3,18 @@ package com.example.interactivitydemo;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     public void clickFunction(View view) {
+        EditText nameEditText = (EditText) findViewById(R.id.nameEditText);
+
         Log.i("Info", "It worked!!");
+
+        Log.i("Values", nameEditText.getText().toString());
     }
 
     @Override
---
+++ the_complete_android_oreo_developer_course_build_23_apps/logindemo/app/src/main/java/com/example/com/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.com;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void login(View view) {
+        EditText usernameEditText = (EditText) findViewById(R.id.usernameEditText);
+        EditText passwordEditText = (EditText) findViewById(R.id.passwordEditText);
+
+        Log.i("Info", "Button pressed");
+
+        Log.i("Username", usernameEditText.getText().toString());
+        Log.i("password", passwordEditText.getText().toString());
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
