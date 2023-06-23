cd90d7286582a4a097ddb35c06ac84a3c36af7e5
Arnold Martinez <arnoldmartinez@criptext.com>
2023-06-22 23:01:37 -0600

    Dynamic values & list in JSX

--- react_front_to_back/feedback-app/src/App.js
+++ react_front_to_back/feedback-app/src/App.js
@@ -1,14 +1,25 @@
-// PURE JAVASCRIPT
-//import React from 'react'
 function App() {
-    // PURE JAVASCRIPT
-    /*return React.createElement('div', { className: 'container'},
-        React.createElement('h1', {}, 'My App'))*/
+    const title = 'Blog Post'
+    const body = 'This is my blog post'
+    const comments = [
+        { id: 1, text: 'Comment one' },
+        { id: 2, text: 'Comment two' },
+        { id: 3, text: 'Comment three' },
+    ]
 
-    // JSX
     return (
         <div className="container">
-            <h1>My App</h1>
+            <h1>{title}</h1>
+            <p>{body}</p>
+
+            <div className='comments'>
+                <h3>Comments ({comments.length})</h3>
+                <ul>
+                    {comments.map((comment, index) => (
+                        <li key={index}>{comment.text}</li>
+                    ))}
+                </ul>
+            </div>
         </div>
     )
 }
