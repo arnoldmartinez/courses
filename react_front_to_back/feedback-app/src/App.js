9c9db2a7ed0dea535b15924186c66d5347158fd7
Arnold Martinez <arnoldmartinez@criptext.com>
2023-06-22 22:48:53 -0600

    Intro to JSX

--- react_front_to_back/feedback-app/src/App.js
+++ react_front_to_back/feedback-app/src/App.js
@@ -1,5 +1,16 @@
+// PURE JAVASCRIPT
+//import React from 'react'
 function App() {
-    return <h1>Hello from the app component</h1>
+    // PURE JAVASCRIPT
+    /*return React.createElement('div', { className: 'container'},
+        React.createElement('h1', {}, 'My App'))*/
+
+    // JSX
+    return (
+        <div className="container">
+            <h1>My App</h1>
+        </div>
+    )
 }
 
 export default App
